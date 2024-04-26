8727b9b6a8a8dc0c6211d2b8b9ae655d2dddb3f7
unknown <jayantjathar2000@gmail.com>
2024-04-26 15:58:36 +0530

    setup1

--- setup.py
+++ setup.py
@@ -11,9 +11,10 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n"," ")for req in requirements]
-
-    if HYPHEN_E_DOT in requirements:
-        requirements.remove(HYPHEN_E_DOT)
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
 
 setup(
 name='mlproject',
